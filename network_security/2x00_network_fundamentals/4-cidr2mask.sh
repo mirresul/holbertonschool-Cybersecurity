b17cd616ed22aa3e1ac64ab705ab34ec12d81014
mirresul <13771@holbertonstudents.com>
2026-05-20 07:26:20 -0400

    j

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$(( $1==0 ? 0 : (0xFFFFFFFF<<(32-$1))&0xFFFFFFFF )); printf "%d.%d.%d.%d\n" $(( (mask>>24)&255 )) $(( (mask>>16)&255 )) $(( (mask>>8)&255 )) $(( mask&255 ))
+b=$(printf "%0${1}d" | tr '0' '1'; printf "%0$((32-$1))d"); printf "%d.%d.%d.%d\n" $(bc <<< "ibase=2; ${b:0:8}") $(bc <<< "ibase=2; ${b:8:8}") $(bc <<< "ibase=2; ${b:16:8}") $(bc <<< "ibase=2; ${b:24:8}")
